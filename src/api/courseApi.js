9165b5d29f0052d99a3cf2ef9f2cd32a39b3e8d8
Pixel-Daze <1226093323@qq.com>
2017-08-15 17:40:00 +0800

    新增课程列表

--- src/api/courseApi.js
+++ src/api/courseApi.js
@@ -1,1 +1,7 @@
 // 课程相关api
+import { fetch,postData } from '../service/httpService'
+import { baseUrl } from '../config/env'
+
+export function getCourseList(body){
+	return fetch('../static/json/CourseList.json')
+}
