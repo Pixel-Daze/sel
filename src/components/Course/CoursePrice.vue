f71be0d10dadb868af4c09baf92a3044519d8a7f
Pixel-Daze <1226093323@qq.com>
2017-09-27 18:01:41 +0800

    修改付款

--- src/components/Course/CoursePrice.vue
+++ src/components/Course/CoursePrice.vue
@@ -77,16 +77,31 @@
 			    	name:vm.courseInfo.name,
 			    	course_id:vm.courseInfo.course_id,
 			    	price:vm.courseInfo.price,
-			    	openid:vm.getCookie('openid')
+			    	openid:vm.getCookie('openid'),
+			    	user_id:vm.getMsg('base','userInfo').user_id
 			    }
 			    api.coursePay(body).then(resp=>{
-			    	console.log(resp)
+			    	if(resp.data.res=='0'){
+			    		vm.openPay(resp.data.data)
+			    	}
 			    })
 				// vm.$vux.alert.show({
 				// 	title: '提示',
 				// 	content: '付费课程暂未开放，敬请期待'
 				// })
 
+			},
+			openPay(data){
+				wx.chooseWXPay({
+				    timestamp: parseInt(data.TimeStamp), 
+				    nonceStr: data.Nonce_str, // 支付签名随机串，不长于 32 位
+				    package: 'prepay_id='+data.Prepay_id, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+				    signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+				    paySign: data.Sign, // 支付签名
+				    success: function (res) {
+				        // 支付成功后的回调函数
+				    }
+				});
 			}
 		},
 		mounted(){
